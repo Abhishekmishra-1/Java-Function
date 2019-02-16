66e8957a7e21325365e4089ab5ddfebcd9764c40
Abhishek Kumar Mishra <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-02-16 20:33:50 +0530

    Update SumOfTwoNumbersAsString.java

--- SumOfTwoNumbersAsString.java
+++ SumOfTwoNumbersAsString.java
@@ -6,15 +6,15 @@ class SumOfTwoNumbersAsString
 		Scanner in = new Scanner(System.in); 
 		String inp1 = in.nextLine(); 
 		String inp2 = in.nextLine();
-    	StringBuilder sb1 = new StringBuilder(inp1);
-    	StringBuilder sb2 = new StringBuilder(inp2);
+    	        StringBuilder sb1 = new StringBuilder(inp1);
+    	        StringBuilder sb2 = new StringBuilder(inp2);
 		StringBuilder result = new StringBuilder("");
 		int carry = 0;
 		int Num1Len = sb1.length()-1;
 		int Num2Len = sb2.length()-1;
 		int sum = 0, rem = 0, idx1 = 0, idx2 = 0;
 		sb1.reverse();
-    	sb2.reverse();
+    	        sb2.reverse();
 		while((idx1 <= Num1Len) && (idx2 <= Num2Len))
 		{
 		    sum =(sb1.charAt(idx1) - '0') + (sb2.charAt(idx2)- '0')+ carry;
@@ -23,16 +23,16 @@ class SumOfTwoNumbersAsString
 		    char num = (char)(rem + '0');
 		    result = result.append(num);
 		    idx1++;
-            idx2++;
+                    idx2++;
 		}
 		while(idx1 <= Num1Len)
 		{
         	sum =(sb1.charAt(idx1) -'0') + carry;
     		carry = sum / 10;
     		rem = sum % 10;
-  		    char num = (char)(rem + '0');
-		    result = result.append(num);
-            idx1++;
+  		char num = (char)(rem + '0');
+		result = result.append(num);
+                idx1++;
         }
         while(idx2 <= Num2Len)
 		{
