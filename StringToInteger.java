9f545a85a460632790f2a53ec91f89090cffcf0a
Abhishek Kumar Mishra <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-02-16 20:29:22 +0530

    Update StringToInteger.java

--- StringToInteger.java
+++ StringToInteger.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class StringToInteger {
 	public static void main (String[] args) {
     	Scanner sc = new Scanner(System.in);
-      System.out.println("Enter a string: ");
+        System.out.println("Enter a string: ");
         String str = sc.next();
         int str_len = str.length();
         int n = 0, idx = 0;
