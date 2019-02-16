6a6cff3fa0f0a9633d17fbfefebc7a81f78595a6
Abhishek Kumar Mishra <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-02-16 20:38:58 +0530

    Update ConvertingStringtoFloat.java

--- ConvertingStringtoFloat.java
+++ ConvertingStringtoFloat.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class ConvertingStringtoFloat {
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string: ");
         String s = input.nextLine();
         int length = s.length();
         double afterDecimal = 0;
