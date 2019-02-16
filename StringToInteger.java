a6eefc830536521d623373cd551872db2aaa1198
Abhishek Kumar Mishra <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-02-16 20:28:42 +0530

    Create StringToInteger.java

---
+++ StringToInteger.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class StringToInteger {
+	public static void main (String[] args) {
+    	Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a string: ");
+        String str = sc.next();
+        int str_len = str.length();
+        int n = 0, idx = 0;
+        while(idx < str_len)
+        {
+           if((str.charAt(idx) >= 48) && (str.charAt(idx) <= 57))
+           {
+            n = (n * 10) + (str.charAt(idx) - 48);
+            idx++;
+           }
+        }
+        System.out.print(n);
+	}
+}
