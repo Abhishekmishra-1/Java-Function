a2e8971d111e99b63746446d77489a67b3f7f639
Abhishek Kumar Mishra <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-02-16 20:37:15 +0530

    Update ConvertingIntegertoString.java

--- ConvertingIntegertoString.java
+++ ConvertingIntegertoString.java
@@ -4,6 +4,7 @@ class ConvertingIntegertoString
     public static void main(String args[])
     {
         Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number: ");
         int n = in.nextInt();
         int temp = n;
         int count = 0;
