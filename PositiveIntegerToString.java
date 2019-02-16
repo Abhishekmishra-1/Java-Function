0d5bb14fffd24bbe0c189d6c5c297fa9f6acda0b
Abhishek Kumar Mishra <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-02-16 20:30:38 +0530

    Create PositiveIntegerToString.java

---
+++ PositiveIntegerToString.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class PositiveIntegerToString
+{
+  public static void main(String args[])
+  {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int temp = n;
+    int count = 0;
+    while(n > 0)
+    {
+        count++;
+        n = n/10;
+    }
+    char str[] = new char[count];
+    count = count - 1;
+    while(temp > 0)
+    {
+      char ch = (char)((temp % 10) + '0');
+      str[count] = ch;
+      temp /= 10;
+      --count;   
+    }
+    System.out.print(str);
+  }
+}
