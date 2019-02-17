3036e7150993f236e28582c97d7ffc8339164296
Abhishek Kumar Mishra <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-02-17 23:53:50 +0530

    Update RemoveDuplicate.java

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -5,6 +5,7 @@ class RemoveDuplicate
 	public static void main(String[] args)
 	{
 	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter a string: ");
 	    String input = sc.nextLine();	
 		String[] words=input.split(" ");	
 		for(int i=0;i<words.length;i++)			
