412badc66d721ce5b525238a18c2c7c332d8439d
Abhishek Kumar Mishra <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-02-17 23:46:41 +0530

    Create ConvertToCamelCase.java

---
+++ ConvertToCamelCase.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class ConvertToCamelCase
+{
+   public static void main(String args[])
+   {
+       Scanner scan = new Scanner(System.in);
+       String str = scan.nextLine();
+       StringBuilder sb = new StringBuilder(str);
+       StringBuilder result = new StringBuilder("");
+       int sb_len = sb.length();
+       for(int index = 0; index <= sb_len - 1; index++)
+       {
+           if(sb.charAt(index) != ' ')
+           {
+               result.append(sb.charAt(index));
+           }
+        else if(sb.charAt(index) == ' ')
+        {
+               char ch = sb.charAt(index + 1);
+               ch = (char)(ch - 32);
+               result.append(ch);
+               index++;
+           }
+       }
+       System.out.print(result);
+   }
+}
