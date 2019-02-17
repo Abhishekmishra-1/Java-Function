6a22cb8f16e93d797037079edbef284e298cd776
Abhishek Kumar Mishra <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-02-17 23:48:52 +0530

    Create SortCharacter.java

---
+++ SortCharacter.java
@@ -0,0 +1,33 @@
+import java.util.Scanner; 
+class SortCharacter
+{ 
+ public static void main (String[] args)
+ {
+    Scanner in = new Scanner(System.in);
+    String str = in.nextLine();
+    StringBuilder string = new StringBuilder(str);
+    int str_len = string.length();
+    for (int index = 0; index < str_len; index++)
+    {
+        if((string.charAt(index) >= 'A') && (string.charAt(index) <= 'Z'))
+        {
+            char ch =(char)(string.charAt(index) + 32);
+            string.setCharAt(index, ch);
+        }
+    }
+    for(int index1 = 0; index1 < str_len; index1++)
+    {
+        for(int index2 = 0; index2 < index1; index2++)
+        {
+            if(string.charAt(index2) >= string.charAt(index1))
+            {
+                char temp = string.charAt(index2);
+                string.setCharAt(index2, string.charAt(index1));
+                string.setCharAt(index1, temp);
+            }
+        }
+    }
+    System.out.println(string);
+ }
+}
++ 
