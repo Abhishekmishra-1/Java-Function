fb2c201081ae58ded4256cbb9f0822436287bf15
Abhishek Kumar Mishra <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-02-18 23:20:56 +0530

    Create NonContiguously.java

---
+++ NonContiguously.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class NonContiguously
+{
+   public static void main (String[] args) 
+   {
+       Scanner in = new Scanner(System.in);
+       String sentence = in.nextLine();
+       String word = in.nextLine();
+       int sentence_len = sentence.length();
+       int word_len = word.length();
+       boolean is_word_found = true;
+       int index2 = 0;
+       for(int index1 = 0; index1 < word_len; index1++)
+       {
+           boolean is_curr_char_found = false;
+           while(index2 < sentence_len)
+           {
+               if(sentence.charAt(index2) == word.charAt(index1))
+               {
+                   is_curr_char_found = true;
+                   index2++;
+                   break;
+               }
+               index2++;
+           }
+           if(is_curr_char_found == false)
+           {
+               is_word_found = false;
+               break;
+           }
+       }
+       if(is_word_found == true)
+       {
+           System.out.print("Found");
+       }
+       else
+       {
+           System.out.print("Not Found");
+       }
+   }
+}
