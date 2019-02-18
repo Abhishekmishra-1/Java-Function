9cd061c54da131ba607baac60d951dc658d5213f
Abhishek Kumar Mishra <42329734+Abhishekmishra-1@users.noreply.github.com>
2019-02-18 23:28:57 +0530

    Update StringPattern.java

--- StringPattern.java
+++ StringPattern.java
@@ -4,6 +4,7 @@ public class StringPattern
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
         String str = sc.nextLine();
         int n = str.length();
         int mid = n/2;
