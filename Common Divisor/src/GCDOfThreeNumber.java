f7eb8702b336c5b208b0d616e42d298117636f74
Arise <Arise@192.168.0.9>
2019-02-23 19:33:13 +0530

    Program created for GCD of three numbers

---
+++ Common Divisor/src/GCDOfThreeNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class GCDOfThreeNumber {
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter three numbers: ");
+		int n1 = in.nextInt();
+		int n2 = in.nextInt();
+		int n3 = in.nextInt();
+		int result = GCD(n1,n2);
+		System.out.println(GCD(result,n3));
+	}
+	public static int GCD(int num1,int num2)
+	{
+		int min = 0;
+		if(num1>num2) {
+			min = num2;
+		}
+		else {
+			min = num1;
+		}
+		while(min>=1)
+		{
+			if((num1%min==0) && (num2%min==0))
+			{
+				return min;
+			}
+			min--;
+		}
+		return 0;
+	}
+}
