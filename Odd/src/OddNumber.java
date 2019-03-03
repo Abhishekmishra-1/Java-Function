38afad87cf9fc14b01bc701ac1da38aadd3ea673
Arise <Arise@192.168.0.9>
2019-03-03 08:24:07 +0530

    program created

--- Odd/src/OddNumber.java
+++ Odd/src/OddNumber.java
@@ -1,8 +1,19 @@
-
+import java.util.Scanner;
 public class OddNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int n = in.nextInt();
+		for(int i = 1;i<=n;i++)
+		{
+			if(i%2==1) {
+				System.out.println(i);
+			}
+			else {
+				i=i+1;
+			}
+		}
 
 	}
 
